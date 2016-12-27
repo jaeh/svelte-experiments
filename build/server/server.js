b651f531c82bf4ac61525078beb20207e275a386
Jascha Ehrenreich <jascha@jaeh.at>
2016-12-27 18:37:20 +0100

    first commit

---
+++ build/server/server.js
@@ -0,0 +1,36 @@
+import path from 'path'
+import express from 'express'
+
+import {
+  devMiddleware,
+  hotMiddleware,
+} from './middleware/webpack-dev-middleware'
+
+const app = express()
+
+const PORT = 3000
+
+app.use(express.static(path.join(process.cwd(), 'public')))
+
+const env = app.get('env')
+
+if (env !== 'production') {
+  app.use(devMiddleware)
+  app.use(hotMiddleware)
+}
+else {
+  app.use(express.static(path.join(process.cwd(), 'bundle')))
+}
+
+app.listen(
+  PORT,
+  err =>
+    err
+      ? console.error(err)
+      : console.log(`Server running @ localhost:${ PORT }`)
+)
+
+process.on('uncaughtException', (err) => {
+  console.error(err)
+  process.exit()
+})
